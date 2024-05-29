dbea34467a5abe24406f22e17c8e21a5ce855ab1
Xinyu Li <xinyuli@uber.com>
2024-05-29 15:45:29 -0700

    add force wait 120 s

--- src/components/attendant/instruction/InstructionAlmostReadyToStart.js
+++ src/components/attendant/instruction/InstructionAlmostReadyToStart.js
@@ -1,60 +1,80 @@
 import {
-    Container, Box, Typography, Button,    Grid,
+    Container, Box, Typography, Button, Grid,
 } from "@mui/material";
 import React from 'react';
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom"
+import { loginAttendant } from "../../../slices/attendantSlice";
 // import { xpConfigS } from "../../../slices/gameSlice";
 import image12 from "../../../assets/12.png";
 import image13 from "../../../assets/13.png";
 import image14 from "../../../assets/14.png";
-
+import TrainingTimer from '../trial/TrainingTimer';
+import { useState, useEffect } from "react";
+import { getAttendant, updateAttendant } from '../../../database/attendant';
 
 const InstructionHowToPlayPage = () => {
     const { alias } = useParams();
-    // const xpConfig = useSelector(xpConfigS);
+    const [enable, setEnable] = useState(false);
+    const [fetched, setFetched] = useState(false);
+    const loginAttendantS = useSelector(loginAttendant);
 
-    // useEffect(() => {
-    //     // Scroll to the top of the page when the component is mounted
-    //     window.scrollTo(0, 0);
-    // }, []); // Empty dependency array means this effect runs only once on mount
+    const fetchData = async () => {
+        const attendant = await getAttendant(loginAttendantS.id);
+        setEnable(attendant.isDoneQuizStartWaiting || false)
+        setFetched(true)
+    }
+
+    useEffect(() => {
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
+    const onFinish = async () => {
+        await updateAttendant(loginAttendantS.id, { isDoneQuizStartWaiting: true });
+        setEnable(true)
+    }
 
     return (
-        <Container maxWidth="lg">
+        <>
+            <Container maxWidth="lg">
 
-            <Grid container justifyContent="center" alignItems="center" sx={{ my: 3 }}>
-                <Grid item xs={12}>
-                    <Typography variant="h4" align="center" >
-                        Almost ready to start!
-                    </Typography>
+                <Grid container justifyContent="center" alignItems="center" sx={{ my: 3 }}>
+                    <Grid item xs={12}>
+                        <Typography variant="h4" align="center" >
+                            Almost ready to start!
+                        </Typography>
+                    </Grid>
                 </Grid>
-            </Grid>
 
-            <Grid container alignItems="center" sx={{ my: 1 }}>
-                <Grid item xs={12}>
-                    <Typography variant="h6" sx={{ my: 2 }}>
-                        Here is an example to quickly remind you how the payoff works in the dangerous zone. Please double-check that all is clear to you, and if in doubt of anything, please ask!
-                    </Typography>
+                <Grid container alignItems="center" sx={{ my: 1 }}>
+                    <Grid item xs={12}>
+                        <Typography variant="h6" sx={{ my: 2 }}>
+                            Here is an example to quickly remind you how the payoff works in the dangerous zone. Please double-check that all is clear to you, and if in doubt of anything, please ask!
+                        </Typography>
 
+                    </Grid>
                 </Grid>
-            </Grid>
 
-            <Grid container alignItems="center" sx={{ my: 5 }}>
-                <Grid item xs={5} sx={{ textAlign: "center" }}>
-                    <Box component="img" alt="" src={image12} sx={{ width: '100%' }} />
+                <Grid container alignItems="center" sx={{ my: 5 }}>
+                    <Grid item xs={5} sx={{ textAlign: "center" }}>
+                        <Box component="img" alt="" src={image12} sx={{ width: '100%' }} />
+                    </Grid>
+                    <Grid item xs={7} sx={{ textAlign: "center" }}>
+                        <Box component="img" alt="" src={image13} sx={{ width: '100%' }} />
+                        <Box component="img" alt="" src={image14} sx={{ width: '100%' }} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={7} sx={{ textAlign: "center" }}>
-                    <Box component="img" alt="" src={image13} sx={{ width: '100%' }} />
-                    <Box component="img" alt="" src={image14} sx={{ width: '100%' }} />
-                </Grid>
-            </Grid>
 
-            <Box textAlign="center" sx={{ my: 10 }}>
-                <Button component={Link} variant="contained" size="large" to={`/xp/${alias}/instruction-ready`} sx={{ mx: 2 }}>Prev</Button>
-                <Button component={Link} variant="contained" size="large" to={`/xp/${alias}/start-quiz`} sx={{ mx: 2 }}>Next</Button>
-            </Box>
-        </Container>
-    )
+                <Box textAlign="center" sx={{ my: 10 }}>
+                    <Button component={Link} variant="contained" size="large" to={`/xp/${alias}/instruction-ready`} sx={{ mx: 2 }} >Prev</Button>
+                    <Button component={Link} variant="contained" size="large" to={`/xp/${alias}/start-quiz`} sx={{ mx: 2 }} disabled={!fetched || (fetched && !enable)}>Next</Button>
+                </Box>
+            </Container>
+
+            {(!fetched || (fetched && !enable)) && <TrainingTimer trainingSessionSeconds={120} onFinish={onFinish} text={"Time left"} />}
+        </>)
 }
 
 export default InstructionHowToPlayPage
